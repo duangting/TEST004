4d10200aaf9627216c8a21ab1b7a155766d1907a
lixiang <2971966792@qq.com>
2018-11-19 22:41:58 +0800

    修改setting源文件-DEFAULT_PAGINATION_CLASS:rest_framework.pagination.PageNumberPagination

--- apps/employee/views.py
+++ apps/employee/views.py
@@ -7,20 +7,28 @@ from rest_framework.views import APIView
 
 from .models import Employee
 from .serializers import serializers_Employee
+from rest_framework import mixins
+from rest_framework import generics
 #CBV模式
-class employee_view(APIView):
-    def get(self,request):
-        all_employee=Employee.objects.all()
-        ser=serializers_Employee(all_employee,many=True)
+class employee_view(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+                        ):
 
-        return Response(ser.data) #需要使用restframeword的Response
+    queryset = Employee.objects.all()
+    serializer_class = serializers_Employee
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs) #需要使用restframeword的Response
 
-    def post(self,request):
-        serializer=serializers_Employee(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            print(serializer.data)
-            return  Response({"msg":"请求成功",'code':'201'},status=status.HTTP_200_OK)
-        return Response(serializer.error_messages,status=status.HTTP_404_NOT_FOUND)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    # def post(self,request):
+    #     serializer=serializers_Employee(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         print(serializer.data)
+    #         return  Response({"msg":"请求成功",'code':'201'},status=status.HTTP_200_OK)
+    #     return Response(serializer.error_messages,status=status.HTTP_404_NOT_FOUND)
 
 
