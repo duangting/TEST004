dcc8f1ca348558d27d62a6779359c3885ea6e054
lixiang <2971966792@qq.com>
2018-11-19 21:31:39 +0800

    增加post方法和ModelSerializer

--- apps/employee/serializers.py
+++ apps/employee/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
-
+from .models import Employee
 
 #做model的映射
-class serializers_Employee(serializers.Serializer):
-    id=serializers.IntegerField(read_only=True)
-    name=serializers.CharField()
-    email=serializers.EmailField()
-
+class serializers_Employee(serializers.ModelSerializer):
+    # id=serializers.IntegerField(read_only=True)
+    # name=serializers.CharField()
+    # email=serializers.EmailField()
+    #
+    # #重写create方法
+    # def create(self, validated_data):
+    #     return Employee.objects.create(**validated_data)
+    class Meta:
+        model=Employee
+        fields=('__all__')
--- apps/employee/views.py
+++ apps/employee/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -13,4 +14,12 @@ class employee_view(APIView):
         ser=serializers_Employee(all_employee,many=True)
 
         return Response(ser.data) #需要使用restframeword的Response
+
+    def post(self,request):
+        serializer=serializers_Employee(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return  Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.error_messages,status=status.HTTP_404_NOT_FOUND)
+
 
