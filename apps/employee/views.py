3df3cecc0a1bff3cae90c4c252a1c290bb88862f
lixiang <2971966792@qq.com>
2018-11-21 21:19:18 +0800

    ceshi github

--- apps/employee/views.py
+++ apps/employee/views.py
@@ -14,10 +14,11 @@ from rest_framework import generics
 #CBV模式
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 11
+    page_size = 10
     page_size_query_param = 'page_size'
     page_query_param = 'pg'
     max_page_size = 1000
+
 class employee_view(
 ListAPIView,
 CreateAPIView,
--- test004/settings.py
+++ test004/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-REST_FRAMEWORK= {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
-}
+# REST_FRAMEWORK= {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+#     'PAGE_SIZE': 9,
+# }
